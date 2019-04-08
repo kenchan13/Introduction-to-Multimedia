bcd73a275f6b3ab545e67b7115a4583dd71a2bbb
Ken Chan <kenchan13@gmail.com>
2019-04-08 21:41:26 +0800

    finish 1a

---
+++ Homework/hw1/q1a.m
@@ -0,0 +1,104 @@
+clc;
+clear all;
+img1 = './data/cat3_LR.png';
+rgbImage = imread(img1);
+
+[ImgHeight, ImgWidth, Layers] = size(rgbImage);
+n = 2;
+
+blockSizeHeight = 8;
+blockSizeWidth = 8;
+
+% Figure out the hight of each block.
+NumOfBlockRows = floor(ImgHeight / blockSizeHeight);
+blockVectorR = [blockSizeHeight * ones(1, NumOfBlockRows), rem(ImgHeight, blockSizeHeight)];
+
+% Figure out the width of each block.
+NumOfBlockCols = floor(ImgWidth / blockSizeWidth);
+blockVectorC = [blockSizeWidth * ones(1, NumOfBlockCols), rem(ImgWidth, blockSizeWidth)];
+
+% Divide YIQImage into n*n pixel blocks
+rgbBlocks = mat2cell(rgbImage, blockVectorR, blockVectorC, Layers);
+
+% Size of rgbBlock
+size = blockSizeHeight;
+
+% Implement 1D DCT basis
+U_temp = zeros(size);
+for c = 0 : size-1
+    for r = 0 : size-1
+        if (c>0)
+            a0 = sqrt(2/size);
+        else
+            a0 = 1/sqrt(size);
+        end
+        U_temp(r+1, c+1) = a0*cos((2*r+1)*c*pi/(2*size));
+    end
+end
+
+% Calculate 2D DCT "U" by using 1D DCT basis
+U = [];
+for k = 1 : size
+    for l = 1 : size
+        % fprintf("k:%d, l:%d\n", k, l);
+        U = [U {U_temp(:,k) * U_temp(:,l)'}];
+    end
+end
+U = reshape(U, [size, size])';
+
+% DCT process
+% S is the YIQBlock source (1 block)
+% TBlocks is sets of amplitudes of frequency components for Image
+
+TBlocks = mat2cell(zeros(ImgHeight, ImgWidth, Layers), blockVectorR, blockVectorC, Layers);
+
+for x = 1: NumOfBlockRows
+    for y = 1: NumOfBlockCols
+        for z = 1: Layers
+            S = rgbBlocks{x, y}(:,:,z); % For each color
+            S = double(S);
+            tempT = zeros(size);
+            for k = 1 : size
+                for l = 1 : size
+                    tempT(k,l) = sum(U{k,l}.*S, 'all');
+                end
+            end
+            % We only need top-left n*n data
+            % Cut tempT and expand it with 0
+            tempT = tempT(1:n, 1:n);
+            tempT(blockSizeHeight, blockSizeHeight) = 0;
+            TBlocks{x, y}(:,:,z) = tempT;
+            
+            % Check result
+            %{
+            if round(dct2(rgbBlocks{x,y}(:,:,z)), 4) ~= round(tempT(:, :), 4)
+                fprintf("Error!: x=%d y=%d, z=%d\n", x, y,z), end
+            %}
+        end
+    end
+end
+
+% DCT inverse
+for x = 1: NumOfBlockRows
+    for y = 1: NumOfBlockCols
+        for z = 1: Layers
+            tempT = TBlocks{x, y}(:,:,z);
+            sum = zeros(size);
+            for i = 1: size
+                for j = 1: size
+                    sum = sum + tempT(i,j)*U{i,j};
+                end
+            end
+            rgbBlocks{x,y}(:,:,z) = sum;
+        end
+    end
+end
+
+
+% Convert rgbBlocks back to rgbImageFinal
+rgbImageFinal = cell2mat(rgbBlocks);
+
+rgbImageInt = uint8(rgbImageFinal);
+imshow(round(rgbImageInt));
+
+fprintf("Finish\n")
